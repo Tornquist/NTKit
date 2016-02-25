7a574e6ce9a5ffc98d9c930412a4731fc007d2eb
Nathan Tornquist <Tornquist@live.com>
2016-02-24 22:50:09 -0600

    Added listener for touch events, and linked NTTiles to NTTileView

--- NTTileView/NTTile.swift
+++ NTTileView/NTTile.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 public class NTTile: UIView {
+    public var parentTileView: NTTileView?
+    
     public func anchorPoint() -> CGPoint {
         return CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+    }
+    
+    public override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // TODO: Check to make sure single touch started and ended in visible part of tile
+        if let tileIndex = parentTileView?.getTileIndex(withTile: self) {
+            parentTileView?.expand(tileWithIndex: tileIndex)
+        }
     }
 }
--- NTTileView/NTTileView.swift
+++ NTTileView/NTTileView.swift
@@ -90,8 +90,36 @@ public class NTTileView: UIView {
         newView.clipsToBounds = true
         self.addSubview(newView)
         newView.addSubview(tile)
+        tile.parentTileView = self
         
         views.append(newView)
         tiles.append(tile)
+    }
+    
+    /**
+     Given an internal index of a specific tile, this method is used
+     to expand that tile.  The actual action performed will depend
+     on the layouting engine used
+     */
+    public func expand(tileWithIndex tileIndex: Int) {
+        NSLog("Expand Tile \(tileIndex)")
+    }
+    
+    /**
+     This method will return the index of a current tile.  Depending
+     on the layout engine used, the tile index may not correspond to the
+     actual on-screen layout.  This is an internal index used by NTTileView
+     to manage the tiles and views they are displayed within.
+     
+     This index can be used to perform operations (such as expand)
+     on a specific tile.
+     */
+    public func getTileIndex(withTile tile: NTTile) -> Int? {
+        for (i, t) in tiles.enumerate() {
+            if t === tile {
+                return i
+            }
+        }
+        return nil
     }
 }
