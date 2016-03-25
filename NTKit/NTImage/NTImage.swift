e2b6928cb55ae2c6f5006b799d38df30ffd517fe
Nathan Tornquist <Tornquist@live.com>
2016-03-25 15:23:16 -0500

    Added blank NTImage to NTKit

---
+++ NTKit/NTImage/NTImage.swift
@@ -0,0 +1,13 @@
+//
+//  NTImage.swift
+//  NTKit
+//
+//  Created by Nathan Tornquist on 3/25/16.
+//  Copyright © 2016 Nathan Tornquist. All rights reserved.
+//
+
+import UIKit
+
+class NTImage: UIImage {
+    
+}
